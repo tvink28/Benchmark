6009f589917ff25ae57f29797971244db9576ce9
@foxstudent103527 <pistron3000@gmail.com>
2023-02-27 18:19:54 +0600

    got rid of duplicates 2

--- app/src/main/java/com/example/task2/ui/benchmark/BaseFragment.java
+++ app/src/main/java/com/example/task2/ui/benchmark/BaseFragment.java
@@ -2,7 +2,9 @@ package com.example.task2.ui.benchmark;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.PopupWindow;
 import android.widget.TextView;
@@ -10,6 +12,8 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.task2.R;
 import com.google.android.material.textfield.TextInputEditText;
@@ -21,6 +25,35 @@ public class BaseFragment extends Fragment implements View.OnFocusChangeListener
     protected View errorView;
     protected PopupWindow errorPopup;
 
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState
+    ) {
+
+        return inflater.inflate(R.layout.fragment_common, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+
+        RecyclerView recyclerView = view.findViewById(R.id.rv);
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
+        recyclerView.setAdapter(adapter);
+
+        textInputEditText = view.findViewById(R.id.editText);
+        Button buttonStopStart = view.findViewById(R.id.btnStopStart);
+        errorView = inflater.inflate(R.layout.error, null);
+        errorPopup = new PopupWindow(errorView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        errorPopup.setFocusable(true);
+        textInputEditText.setOnFocusChangeListener(this);
+        buttonStopStart.setOnClickListener(this);
+
+    }
 
     public void onFocusChange(View v, boolean hasFocus) {
         if (!hasFocus) {
--- app/src/main/java/com/example/task2/ui/benchmark/MapsFragment.java
+++ app/src/main/java/com/example/task2/ui/benchmark/MapsFragment.java
@@ -1,17 +1,6 @@
 package com.example.task2.ui.benchmark;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.PopupWindow;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.task2.R;
 import com.example.task2.models.CellOperation;
@@ -28,35 +17,6 @@ public class MapsFragment extends BaseFragment {
         adapter.setItems(createItemsList());
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_common, container, false);
-
-
-    }
-
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        LayoutInflater inflater = (LayoutInflater) view.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-
-        RecyclerView recyclerView = view.findViewById(R.id.rv);
-        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
-        recyclerView.setAdapter(adapter);
-
-
-        textInputEditText = view.findViewById(R.id.editText);
-        Button buttonStopStart = view.findViewById(R.id.btnStopStart);
-        errorView = inflater.inflate(R.layout.error, null);
-        errorPopup = new PopupWindow(errorView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-
-        errorPopup.setFocusable(true);
-        textInputEditText.setOnFocusChangeListener(this);
-        buttonStopStart.setOnClickListener(this);
-
-    }
 
     private Collection<CellOperation> createItemsList() {
         return Arrays.asList(
