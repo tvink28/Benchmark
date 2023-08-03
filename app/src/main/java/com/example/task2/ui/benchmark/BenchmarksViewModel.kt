5c510d948d26c870bf8e9b5a93903e07eacdfe5a
@foxstudent103527 <pistron3000@gmail.com>
2023-08-03 20:13:52 +0600

    fixes 3

--- app/src/main/java/com/example/task2/ui/benchmark/BenchmarksViewModel.kt
+++ app/src/main/java/com/example/task2/ui/benchmark/BenchmarksViewModel.kt
@@ -10,69 +10,74 @@ import com.example.task2.models.benchmarks.CellOperation
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class BenchmarksViewModel(private val benchmark: Benchmark) : ViewModel() {
 
-	private val cellOperationsLiveData = MutableLiveData<List<CellOperation>>(emptyList())
-	private val allTasksCompletedLiveData = MutableLiveData(true)
-	private val validNumberLiveData = MutableLiveData<Int?>()
+    private val cellOperationsLiveData = MutableLiveData<List<CellOperation>>(emptyList())
+    private val allTasksCompletedLiveData = MutableLiveData(true)
+    private val validNumberLiveData = MutableLiveData<Int?>()
 
-	private var benchmarkJob: Job? = null
+    private var benchmarkJob: Job? = null
 
-	fun getCellOperationsLiveData(): LiveData<List<CellOperation>> = cellOperationsLiveData
-	fun getAllTasksCompletedLiveData(): LiveData<Boolean> = allTasksCompletedLiveData
-	fun getValidNumberLiveData(): LiveData<Int?> = validNumberLiveData
-	fun getNumberOfColumns(): Int = benchmark.getNumberOfColumns()
+    fun getCellOperationsLiveData(): LiveData<List<CellOperation>> = cellOperationsLiveData
+    fun getAllTasksCompletedLiveData(): LiveData<Boolean> = allTasksCompletedLiveData
+    fun getValidNumberLiveData(): LiveData<Int?> = validNumberLiveData
+    fun getNumberOfColumns(): Int = benchmark.getNumberOfColumns()
 
-	fun onButtonClicked(input: String) {
-		val number = input.toIntOrNull()
-		when {
-			benchmarkJob?.isActive == true -> stopBenchmark()
-			number != null -> runBenchmark(number)
-		}
-	}
+    fun onButtonClicked(input: String) {
+        val number = input.toIntOrNull()
+        when {
+            benchmarkJob?.isActive == true -> stopBenchmark()
+            number != null -> runBenchmark(number)
+        }
+    }
 
-	fun validateNumber(input: String) {
-		val number = input.toIntOrNull()
-		val errorMessage = when {
-			number == null -> R.string.error_valid
-			number < 1 -> R.string.error_count
-			else -> null
-		}
-		validNumberLiveData.value = errorMessage
-	}
+    fun validateNumber(input: String) {
+        val number = input.toIntOrNull()
+        val errorMessage = when {
+            number == null -> R.string.error_valid
+            number < 1 -> R.string.error_count
+            else -> null
+        }
+        validNumberLiveData.value = errorMessage
+    }
 
-	fun onCreate() {
-		cellOperationsLiveData.value = benchmark.createItemsList(false)
-	}
+    fun onCreate() {
+        cellOperationsLiveData.value = benchmark.createItemsList(false)
+    }
 
-	private fun runBenchmark(number: Int) {
-		val handler = CoroutineExceptionHandler { _, exception ->
-			exception.printStackTrace()
-		}
+    private fun runBenchmark(number: Int) {
+        val handler = CoroutineExceptionHandler { _, exception ->
+            exception.printStackTrace()
+        }
 
-		val operations = benchmark.createItemsList(true).toMutableList()
-		cellOperationsLiveData.value = ArrayList(operations)
+        val operations = benchmark.createItemsList(true).toMutableList()
+        cellOperationsLiveData.value = ArrayList(operations)
 
-		benchmarkJob = viewModelScope.launch(handler) {
-			operations.mapIndexed { index, cell ->
-				async(Dispatchers.Default) {
-					val operationTime = benchmark.measureTime(cell, number)
-					index to cell.withTime(operationTime)
-				}
-			}.forEach { deferred ->
-				val (position, updatedCell) = deferred.await()
-				operations[position] = updatedCell
-				cellOperationsLiveData.value = ArrayList(operations)
-			}
-			allTasksCompletedLiveData.postValue(true)
-		}
-	}
+        benchmarkJob = viewModelScope.launch(handler) {
+            val jobs = mutableListOf<Job>()
 
-	private fun stopBenchmark() {
-		benchmarkJob?.cancel()
-		cellOperationsLiveData.value = cellOperationsLiveData.value?.map { it.withIsRunning(false) }
-	}
+            operations.withIndex().forEach { (index, cell) ->
+                val childJob = launch(Dispatchers.Default) {
+                    val operationTime = benchmark.measureTime(cell, number)
+                    val updatedCell = cell.withTime(operationTime)
+
+                    withContext(Dispatchers.Main) {
+                        operations[index] = updatedCell
+                        cellOperationsLiveData.value = ArrayList(operations)
+                    }
+                }
+                jobs.add(childJob)
+            }
+            jobs.forEach { it.join() }
+            allTasksCompletedLiveData.value = true
+        }
+    }
+
+    private fun stopBenchmark() {
+        benchmarkJob?.cancel()
+        cellOperationsLiveData.value = cellOperationsLiveData.value?.map { it.withIsRunning(false) }
+    }
 }
